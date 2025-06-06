24bceb5c747f63d13d715ac8f2c81d6b4801672d
drewwilliamsrostron <138461285+drewwilliamsrostron@users.noreply.github.com>
2025-06-07 00:26:26 +0100

    Fix dependency installer

--- run_artibot.py
+++ run_artibot.py
@@ -1,6 +1,8 @@
 """Command line entry point for the trading bot package."""
 
+from artibot.environment import ensure_dependencies
 from artibot.bot_app import run_bot
 
 if __name__ == '__main__':
+    ensure_dependencies()
     run_bot()
